11b0d263e64fcc0fc1ca1ba9af71c8679ada7b1a
Momtchil Momtchev <momtchil@momtchev.com>
2024-03-29 16:26:23 +0100

    make basic web demo

--- test/browser/index.js
+++ test/browser/index.js
@@ -4,20 +4,30 @@ import WASM from '../../lib/wasm.mjs';
 
 console.log('Hello from WASM');
 
+function print(msg) {
+  const div = document.createElement('div');
+  const text = document.createTextNode(msg);
+  div.appendChild(text);
+  document.getElementsByTagName('body')[0].appendChild(div);
+}
+
+print('Loading WASM');
 WASM.then((bindings) => {
-  console.log('WASM loaded and compiled', bindings);
-  const Blob = bindings.Blob;
-  
-  const blob = new Blob(1024);
-  if (!(blob instanceof Blob)) throw new Error('Failed creating a blob');
-  console.log('Created a Blob!');
+  print('WASM loaded and transpiled');
+  console.log('WASM', bindings);
 
-  blob.Fill(17);
-  const ab = blob.Export();
-  if (ab.byteLength !== 1024) throw new Error('Length does not match');
-  console.log('got', ab);
-  const view = new Uint8Array(ab);
-  if (view[17] !== 17) throw new Error('Data does not match');
+  const dbContext = bindings.DatabaseContext.create();
+  const authFactory = bindings.AuthorityFactory.create(dbContext, 'string');
+  const coord_op_ctxt = bindings.CoordinateOperationContext.create(authFactory, null, 0);
+  const authFactoryEPSG = bindings.AuthorityFactory.create(dbContext, 'EPSG');
+  const sourceCRS = authFactoryEPSG.createCoordinateReferenceSystem('4326');
+  const targetCRS = bindings.createFromUserInput('+proj=utm +zone=31 +datum=WGS84 +type=crs', dbContext);
+  const list = bindings.CoordinateOperationFactory.create().createOperations(sourceCRS, targetCRS, coord_op_ctxt);
 
-  document.getElementsByTagName('body')[0].innerHTML = 'Successfully loaded WASM and created a blob';
+  const transformer = list[0].coordinateTransformer();
+  const c0 = new bindings.PJ_COORD;
+  c0.v = [49, 2, 0, 0];
+  const c1 = transformer.transform(c0);
+
+  print(`Successfully converted ${c0.v} from EPSG:4326 to UTM WGS84 -> ${c1.v}`);
 });
