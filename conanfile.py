68c3221098bff8c934c7254e2f335ab41add4577
Momtchil Momtchev <momtchil@momtchev.com>
2024-04-01 11:16:10 +0200

    load the meson options in conan

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,26 @@
 from conan import ConanFile
+import subprocess
+import json
 
 required_conan_version = ">=2.0.0"
 
 class PROJDependencies(ConanFile):
   settings = 'os', 'compiler', 'build_type', 'arch'
 
-  options = {
-    'enable_tiff':    [ True, False ],
-    'enable_curl':    [ True, False ],
-    'inline_projdb':  [ True, False ],
-    'no_async':       [ True, False ]
-  }
-
-  default_options = {
-    'enable_tiff':    True,
-    'enable_curl':    True,
-    'inline_projdb':  True,
-    'no_async':       True
-  }
+  options = {}
+  default_options = {}
+  meson_json = subprocess.Popen(['meson', 'introspect', '--buildoptions', 'meson.build'], shell=False, stdout=subprocess.PIPE)
+  meson_json.wait()
+  data, err = meson_json.communicate()
+  meson_opts = [opt for opt in json.loads(data) if opt['section'] == 'user']
+  for o in meson_opts:
+    # Conan supports only booleans and combos
+    if o['type'] == 'boolean':
+      options[o['name']] = [ True, False ]
+      default_options[o['name']] = o['value']
+    elif o['type'] == 'combo':
+      options[o['name']] = o['choices']
+      default_options[o['name']] = o['value']
 
   generators = [ 'MesonToolchain', 'PkgConfigDeps', 'CMakeDeps' ]
 
