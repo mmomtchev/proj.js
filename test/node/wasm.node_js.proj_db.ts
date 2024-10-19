0a484c14b139184937fdc3f75c89e9e8d580daf6
Momtchil Momtchev <momtchil@momtchev.com>
2024-10-19 12:02:29 +0200

    publishing scripts

--- test/node/wasm.node_js.proj_db.ts
+++ test/node/wasm.node_js.proj_db.ts
@@ -1,5 +1,6 @@
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+import * as process from 'node:process';
 import { fileURLToPath } from 'node:url';
 import { assert } from 'chai';
 import qPROJ from 'proj.js/wasm';
@@ -9,7 +10,9 @@ const PROJ = await qPROJ;
 // when it hasn't been already inlined
 
 async function loadProjDb() {
-  const proj_db_path = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..', '..', 'lib', 'binding', 'proj', 'proj.db');
+  const proj_db_path = process.env.PROJ_DB_PATH ?
+    process.env.PROJ_DB_PATH : 
+    path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..', '..', 'lib', 'binding', 'proj', 'proj.db');
   console.log(`Loading proj.db from ${proj_db_path}`);
   const proj_db_data = await fs.promises.readFile(proj_db_path);
   assert.throws(() => {
