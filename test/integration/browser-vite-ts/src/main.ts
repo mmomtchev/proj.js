25d123d9d97c91a0c468d64169f1702db0887cb4
Momtchil Momtchev <momtchil@momtchev.com>
2025-12-01 20:29:46 +0100

    fix Vite with TypeScript

--- test/integration/browser-vite-ts/src/main.ts
+++ test/integration/browser-vite-ts/src/main.ts
@@ -5,7 +5,33 @@ const assert: Chai.AssertStatic = chai.assert;
 import qPROJ from 'proj.js';
 import type * as PROJ from 'proj.js';
 
+// @ts-ignore
+import proj_db_url from 'proj.js/proj.db?url';
+
+// This loads proj.db into the environment
+// when it hasn't been already inlined
+async function loadProjDb(PROJ: Awaited<typeof qPROJ>) {
+  console.log(`Loading proj.db from ${proj_db_url}`);
+  const proj_db_data = new Uint8Array(await (await fetch(proj_db_url)).arrayBuffer());
+  console.log(`Downloaded ${proj_db_data.length} bytes`);
+  assert.throws(() => {
+    PROJ.loadDatabase('text' as any);
+  }, /Uint8Array/);
+  PROJ.loadDatabase(proj_db_data);
+}
+
 describe('PROJ', () => {
+  before('load proj.db', (done) => {
+    qPROJ.then((PROJ) => {
+      if (!PROJ.proj_js_inline_projdb) {
+        loadProjDb(PROJ).then(() => done()).catch(done);
+      } else {
+        console.log('proj.db is inlined in the WASM bundle');
+        done();
+      }
+    }).catch(done);
+  });
+
   it('PROJ quickstart', () =>
     qPROJ.then((PROJ) => {
       console.time('DatabaseContext.create()');
