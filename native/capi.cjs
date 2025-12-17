1d5bbe073394f7ef1e28d8a8733a67626e906a28
Momtchil Momtchev <momtchil@momtchev.com>
2025-12-17 03:21:46 +0100

    attach the iterators to the WASM module

--- native/capi.cjs
+++ native/capi.cjs
@@ -9,25 +9,7 @@ module.exports = require(path.resolve(__dirname, '..', 'lib', 'binding', `${os.p
 
 
 // Iterators are best implemented in JavaScript
-
-function container_iterator () {
-  const iter = this.iterator();
-  const parent = this;
-  const r = {
-    next() {
-      const v = iter.next();
-      if (v !== null) {
-        // container elements are just pointers in the container
-        // keep a reference to the container, otherwise they may become invalid
-        Object.defineProperty(v, 'parent', { writable: false, value: parent });
-        return { value: v, done: false };
-      }
-      return { done: true };
-    }
-  };
-  Object.defineProperty(r, 'parent', { writable: false, value: parent });
-  return r;
-};
+const container_iterator = require('../lib/capi-iterators.cjs');
 
 module.exports.PROJ_UNIT_INFO_CONTAINER.prototype[Symbol.iterator] = container_iterator;
 module.exports.PROJ_CELESTIAL_BODY_INFO_CONTAINER.prototype[Symbol.iterator] = container_iterator;
--- wasm/capi.mjs
+++ wasm/capi.mjs
@@ -7,7 +7,13 @@
  */
 import bindings from '../lib/binding/emscripten-wasm32/proj_capi.mjs';
 import emnapi from './emnapi.mjs';
+import { container_iterator } from '../lib/capi-iterators.cjs';
 
-const result = emnapi(bindings);
+const result = emnapi(bindings).then((PROJ) => {
+  PROJ.PROJ_UNIT_INFO_CONTAINER.prototype[Symbol.iterator] = container_iterator;
+  PROJ.PROJ_CELESTIAL_BODY_INFO_CONTAINER.prototype[Symbol.iterator] = container_iterator;
+  PROJ.PROJ_CRS_INFO_CONTAINER.prototype[Symbol.iterator] = container_iterator;
+  return PROJ;
+});
 
 export default result;
